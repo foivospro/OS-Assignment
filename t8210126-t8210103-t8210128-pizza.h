560adcef2e793b4bec35881244b0910f5fbb0626
Foivos Proestakis <115726918+foivospro@users.noreply.github.com>
2024-05-04 20:26:20 +0300

    Update t8210126-t8210103-t8210128-pizza.h

--- t8210126-t8210103-t8210128-pizza.h
+++ t8210126-t8210103-t8210128-pizza.h
@@ -27,16 +27,10 @@
 #define DELIVERY_MIN_TIME 5
 #define DELIVERY_MAX_TIME 15
 
-pthread_mutex_t tel_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t cook_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t oven_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t deliverer_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t revenue_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t stats_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t tel_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cook_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t oven_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t deliverer_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t income_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t pizza_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t order_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int total_revenue = 0;
 int margherita_sold = 0;
